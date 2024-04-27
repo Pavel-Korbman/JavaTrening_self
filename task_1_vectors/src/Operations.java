6088a7f6e2ab4f2652f9344f4991532a27c8c2a3
Pavel <pavel@b2profit.ru>
2024-04-27 12:13:25 +0300

    Задачка про вектора по ООП

---
+++ task_1_vectors/src/Operations.java
@@ -0,0 +1,5 @@
+public class Operations {
+    public static double Summ(Vector vector) {
+        return Math.sqrt(Math.pow(vector.getX(), 2) + Math.pow(vector.getY(), 2) + Math.pow(vector.getZ(), 2));
+    }
+}
---
+++ task_1_vectors/src/Vector.java
@@ -0,0 +1,35 @@
+public class Vector {
+    double X;
+    double Y;
+    double Z;
+
+    public Vector(double x, double y, double z) {
+        X = x;
+        Y = y;
+        Z = z;
+    }
+
+    public double getX() {
+        return X;
+    }
+
+    public void setX(double x) {
+        X = x;
+    }
+
+    public double getY() {
+        return Y;
+    }
+
+    public void setY(double y) {
+        Y = y;
+    }
+
+    public double getZ() {
+        return Z;
+    }
+
+    public void setZ(double z) {
+        Z = z;
+    }
+}
